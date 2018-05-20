061ed1768831588ffabd5e041e3d15df7c6c09e7
Benjamin Franklin <31782653+spotme-main@users.noreply.github.com>
2018-05-19 23:19:27 -0400

    Upload of fork ready pool

---
+++ website/config.js
@@ -0,0 +1,9 @@
+var api = "http://pool1.crypto-me.com:8117";
+var poolHost = "pool1.crypto-me.com";
+var irc = "irc.freenode.net/#spotmecoin";
+var email = "support@spotmecoin.com";
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+var blockchainExplorer = "http://explorer.spotmecoin.com/?hash={id}#blockchain_block";
+var transactionExplorer = "http://explorer.spotmecoin.com/?hash={id}#blockchain_transaction";
+var themeCss = "themes/default-theme.css";
